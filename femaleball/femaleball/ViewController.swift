1d6dd794844167635ed5453d136df45b8b0c5bf8
郑少朋 <911917@nd.com>
2019-07-01 19:31:33 +0800

    femaleball

---
+++ femaleball/femaleball/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  femaleball
+//
+//  Created by zsp on 2019/7/1.
+//  Copyright © 2019 woop. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ femaleball/femaleballTests/femaleballTests.swift
@@ -0,0 +1,34 @@
+//
+//  femaleballTests.swift
+//  femaleballTests
+//
+//  Created by zsp on 2019/7/1.
+//  Copyright © 2019 woop. All rights reserved.
+//
+
+import XCTest
+@testable import femaleball
+
+class femaleballTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
